5917003c4e566f474c96b1823e2f897a42f358a8
hsj1023 <hsj1023@hanmail.net>
2018-04-18 14:21:42 +0900

    feat(test): change word

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css';
 
 const MyComponent = () => (
-  <h1>Hello from My Component</h1>
+  <h1>Hello from My Component is edited</h1>
 );
 
 export default MyComponent;
